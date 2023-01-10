5969a0336f93044ba512907c191146a4a548fa92
Eli Ben-Shoshan <ebs@ufl.edu>
2023-01-10 11:15:05 -0500

    Changed to using automatic1111 api

--- code/app.py
+++ code/app.py
@@ -21,6 +21,15 @@ debug = True
 #app.logger.info(f'Debug: {debug}\nToken: {postTokens}')
 auth_token = os.environ['auth_token']
 
+steps = os.environ['steps']
+sampler = os.environ['sampler']
+width = os.environ['width']
+height = os.environ['height']
+checkpoint = os.environ['checkpoint']
+username = os.environ['username']
+password = os.environ['password']
+sdDomain = os.environ['sdDomain']
+
 app = Flask(__name__)
 app.config.from_object("config")
 
@@ -35,19 +44,51 @@ client = Celery(app.name, backend=app.config['CELERY_RESULT_BACKEND'], broker=ap
 @client.task
 def fetch_and_reply(query, channel):
     app.logger.debug("Starting up image processing task")
-    url = 'http://diffusion:5000/predictions'
+    url = f'https://{username}:{password}@{sdDomain}/sdapi/v1/txt2img'
 
     data = {
-        "input": {
-            "prompt": str(query),
-            "width": "512",
-            "height": "512",
-            "num_outputs": "1"
-        }
+        "enable_hr": "false",
+        "denoising_strength": 0,
+        "firstphase_width": 0,
+        "firstphase_height": 0,
+        "hr_scale": 2,
+        "hr_upscaler": "string",
+        "hr_second_pass_steps": 0,
+        "hr_resize_x": 0,
+        "hr_resize_y": 0,
+        "prompt": str(query),
+        "styles": [
+        ],
+        "seed": -1,
+        "subseed": -1,
+        "subseed_strength": 0,
+        "seed_resize_from_h": -1,
+        "seed_resize_from_w": -1,
+        "batch_size": 1,
+        "n_iter": 1,
+        "steps": steps,
+        "cfg_scale": 8,
+        "width": width,
+        "height": height,
+        "restore_faces": "false",
+        "tiling": "false",
+        "negative_prompt": "((((ugly)))), (((duplicate))), ((morbid)), ((mutilated)), extra fingers, mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), blurry, ((bad anatomy)), (((bad proportions))), ((extra limbs)), cloned face, (((disfigured))), gross proportions, (malformed limbs), ((missing arms)), ((missing legs)), (((extra arms))), (((extra legs))), mutated arms, (((long neck))), melting faces, long neck, flat color, flat shading, (bad legs), one leg, extra leg, (bad face), (bad eyes), ((bad hands, bad feet, missing fingers, cropped:1.0)), worst quality, jpeg artifacts, signature, (((watermark))), (username), blurry, ugly, old, wide face, ((fused fingers)), ((too many fingers)), amateur drawing, odd, fat, out of frame, (cloned face:1.3), (mutilated:1.3), (deformed:1.3), (gross proportions:1.3), (disfigured:1.3), (mutated hands:1.3), (bad hands:1.3), (extra fingers:1.3), long neck, extra limbs, broken limb, asymmetrical eyes, necklace, drawn, ((mutilated)), [out of frame], mutated hands, Photoshop, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, mutation, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, body out of frame, blurry, bad art, bad anatomy, 3d render",
+        "eta": 0,
+        "s_churn": 0,
+        "s_tmax": 0,
+        "s_tmin": 0,
+        "s_noise": 1,
+        "override_settings": {
+            "sd_model_checkpoint": str(checkpoint)
+        },
+        "override_settings_restore_afterwards": "false",
+        "script_args": [],
+        "sampler_name": str(sampler),
+        "sampler_index": str(sampler)
     }
 
     image_date = datetime.now().strftime("%Y-%m-%dT%H%M%S.%f")
-    image_name = data["input"]["prompt"].replace(" ","_")
+    image_name = data["prompt"].replace(" ","_")
     uri = image_date + "+" + image_name + ".png"
     filename = outdir + image_date + "+" + image_name + ".png"
 
@@ -56,9 +97,9 @@ def fetch_and_reply(query, channel):
 
     r = requests.post(url, stream=True, json=data)
     if r.status_code == 200:
-        output = r.json()['output'][0]
-        base64string = output.replace("data:image/png;base64,","")
-        image = base64.b64decode(base64string)
+        data = r.json()
+        imageData = data['images'][0]
+        image = base64.b64decode(imageData)
         out_file = open(filename, 'wb')
         out_file.write(image)
         out_file.close()
