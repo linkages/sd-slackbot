fac5dcebed8f3db6173c855a5db82b80e3dc093b
Eli Ben-Shoshan <ebs@ufl.edu>
2023-01-10 10:49:20 -0500

    Added a script that gets all the models.

---
+++ models.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import shutil
+import requests
+import base64
+from datetime import datetime
+
+url = 'https://user:aiIsReallyCool!@ai.benshoshan.com/sdapi/v1/sd-models'
+
+r = requests.get(url)
+if r.status_code == 200:
+    data = r.json()
+    for item in data:
+      print(f"{item['model_name']}: {item['title']}")
+else:
+    print("Something went wrong: [{status}]".format(status=r.status_code))
+
+del r
